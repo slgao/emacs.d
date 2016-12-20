20b24aad276618fc0c5ea0795c465728c511361f
slgao <sleangao@gmail.com>
2016-12-20 17:55:11 +0100

    add features to emacs config

--- lisp/init-better-defaults.el
+++ lisp/init-better-defaults.el
@@ -1,15 +1,51 @@
+(defun indent-buffer ()
+  "Indent the currently visited buffer."
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun indent-region-or-buffer ()
+  "Indent a region if selected, otherwise the whole buffer."
+  (interactive)
+  (save-excursion
+    (if (region-active-p)
+	(progn
+	  (indent-region (region-beginning) (region-end))
+	  (message "Indented selected region."))
+      (progn
+	(indent-buffer)
+	(message "Indented buffer.")))))
+
+;; hippie expand is dabbrev expand on steroids
+(setq hippie-expand-try-functions-list '(try-expand-dabbrev
+                                         try-expand-dabbrev-all-buffers
+                                         try-expand-dabbrev-from-kill
+                                         try-complete-file-name-partially
+                                         try-complete-file-name
+                                         try-expand-all-abbrevs
+                                         try-expand-list
+                                         try-expand-line
+                                         try-complete-lisp-symbol-partially
+                                         try-complete-lisp-symbol))
+
+
 (setq ring-bell-function 'ignore)
-; if init file changed, it will show in the editor
+					; init file changed, it will show in the editor
 (global-auto-revert-mode t)
 
 (require 'recentf)
 (recentf-mode t)
 (setq recentf-max-menu-items 25)
-
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 (delete-selection-mode t)
 
 (setq make-backup-files nil)
 (setq auto-save-default nil)
+
+
+(setq dired-recursive-copies 'always)
+(setq dired-recursive-deletes 'always)
+(put 'dired-find-alternate-file 'disabled nil)
+(require 'dired-x)
+(setq dired-dwim-target t)
 
 (provide 'init-better-defaults)
--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -15,5 +15,9 @@
 (global-set-key (kbd "C-h v") 'counsel-describe-variable)
 
 (global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-M-\\") 'indent-region-or-buffer)
+(global-set-key (kbd "M-/") 'hippie-expand)
+(with-eval-after-load 'dired
+  (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
 
 (provide 'init-keybindings)
