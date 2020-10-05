415dd87f8ebadee79475d30bea634f5aa40edf72
Shulin Gao <Shulin.Gao@eurailscout.com>
2020-10-05 18:30:50 +0200

    remove duplicated package-init

--- init.el
+++ init.el
@@ -7,7 +7,6 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 (require 'org-install)
 (require 'ob-tangle)
