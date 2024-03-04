707cb39999625074cd2c01b56faa4e9412fd862b
sgao0001 <shulin.gao@eurailscout.com>
2024-03-04 13:28:14 +0100

    require 'org-install is obsolete since emacs 26.1

--- init.el
+++ init.el
@@ -8,7 +8,6 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 ;; (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
-(require 'org-install)
 (require 'ob-tangle)
 (org-babel-load-file (expand-file-name "shulins_emacs_config.org" user-emacs-directory))
 (put 'narrow-to-region 'disabled nil)
