ebf0fc14f9b96185d06fb52c6050351e301a86be
slgao <sleangao@gmail.com>
2026-07-14 23:11:18 +0200

    reduce company completion request rate
    idle-delay 0.05 -> 0.1 and minimum-prefix-length 1 -> 2: stop firing an LSP
    completion request after every single keystroke.

--- lisp/custom.el
+++ lisp/custom.el
@@ -8,8 +8,8 @@
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(column-number-mode t)
- '(company-idle-delay 0.05)
- '(company-minimum-prefix-length 1)
+ '(company-idle-delay 0.1)
+ '(company-minimum-prefix-length 2)
  '(company-quickhelp-color-background "#4F4F4F")
  '(company-quickhelp-color-foreground "#DCDCCC")
  '(compilation-message-face 'default)
